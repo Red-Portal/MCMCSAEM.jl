f0c80fc45f89bb00203df544aa9020f1b1c19ea1
Ray Kim <msca8h@naver.com>
2024-02-07 01:50:07 +0000

    add stats logging, add inner mcmc steps

--- src/mcmcsaem.jl
+++ src/mcmcsaem.jl
@@ -13,41 +13,47 @@ function mcmcsaem(
     γ_schedule,
     h          ::Real;
     ad         ::ADTypes.AbstractADType = ADTypes.AutoZygote,
-    mcmc_type     = :ula,
-    callback!     = nothing,
-    show_progress = true
+    mcmc_type         = :ula,
+    callback!         = nothing,
+    show_progress     = true,
+    n_inner_mcmc::Int = 1
 )
-    x    = x₀ isa Vector ? reshape(x₀, (:,1)) : copy(x₀)
-    θ    = copy(θ₀)
-    prog = Progress(T + T_burn; enabled=show_progress, showspeed=true)
+    x     = x₀ isa Vector ? reshape(x₀, (:,1)) : copy(x₀)
+    θ     = copy(θ₀)
+    prog  = Progress(T + T_burn; enabled=show_progress, showspeed=true)
+    stats = Vector{NamedTuple}(undef, T + T_burn)
 
     ∇ℓ_buf = DiffResults.DiffResult(zero(eltype(x)), similar(x, size(x, 1)))
 
     for t = 1:T_burn
         x, _ = mcmc_transition!(rng, ad, Val(mcmc_type), model, x, θ, h, ∇ℓ_buf)
 
-        stats = (t=t, state=:burn_markovchain)
-        pm_next!(prog, stats)
+        stats[t] = (t=t, state=:burn_markovchain)
+        pm_next!(prog, stats[t])
     end
-    S = sufficient_statistic(model, x)
+    S = sufficient_statistic(model, x, θ)
 
     for t = 1:T
+        for _ = 1:n_inner_mcmc
+            x, _ = mcmc_transition!(rng, ad, Val(mcmc_type), model, x, θ, h, ∇ℓ_buf)
+        end
         x, α   = mcmc_transition!(rng, ad, Val(mcmc_type), model, x, θ, h, ∇ℓ_buf)
         V      = mean(x′ -> LogDensityProblems.logdensity(model, x′, θ), eachcol(x))
-        S′      = sufficient_statistic(model, x)
+        S′      = sufficient_statistic(model, x, θ)
         H      = S′ - S
         S_prev = S
         γₜ     = γ_schedule(t)
         S      = S + γₜ*H
         θ      = maximize_surrogate(model, S)
 
-        stats = (t=t, loglike=V, γₜ=γₜ, ΔS=norm(S_prev - S), state=:run_mcmcsaem, acc=α)
+        stat = (t=t, loglike=V, γₜ=γₜ, ΔS=norm(S_prev - S), state=:run_mcmcsaem, acc=α)
 
         if !isnothing(callback!)
-            stat′  = callback!(t, x, θ, stats)
-            stats = !isnothing(stat′) ? merge(stats, stat′) : stats
+            stat′ = callback!(t, x, θ, stat)
+            stat = !isnothing(stat′) ? merge(stat, stat′) : stat
         end
-        pm_next!(prog, stats)
+        stats[t+T_burn] = stat
+        pm_next!(prog, stat)
     end
-    θ, x
+    θ, stats
 end
